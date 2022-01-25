8dd89bd991735f1c6c6e5f41cbd52f29ede3785f
Tejas988 <54373852+Tejas988@users.noreply.github.com>
2022-01-25 15:57:08 +0000

    Deploying to gh-pages from @ Tejas988/tejasghone-portfolio@bfc02c2229626ccffb1832599abeef884e2a9d5f 🚀

---
+++ _next/static/g6rrLzkJz_AEoAtXXw7Rk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-44c2e473f4f94ccaf5de.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
