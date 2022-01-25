76ab087fdee93115d7008dd28dafb33aaa28de17
Tejas988 <tejasghone73@gmail.com>
2022-01-25 19:31:43 +0530

    updates

--- next.config.js
+++ next.config.js
@@ -2,5 +2,7 @@ module.exports = {
 	reactStrictMode: false,
 	images: {
 		domains: ["avatars.githubusercontent.com","play-lh.googleusercontent.com","upload.wikimedia.org","avatars1.githubusercontent.com","drive.google.com"],
-	}
+	},
+	basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
+	assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || "",
 };
