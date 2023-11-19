5d6050b716afd07ef9c1297faf52e3d95483a117
Tejas Ghone <tejasghone@Tejass-MacBook-Air.local>
2023-11-19 23:21:54 +0530

    update portfolio

--- components/ExperienceCard.jsx
+++ components/ExperienceCard.jsx
@@ -38,7 +38,7 @@ const ExperienceCard = ({ data }) => {
 							<img
 								src={process.env.NEXT_PUBLIC_BASE_PATH + data.companylogo}
 								width={"160px"}
-								height={"160px"}
+								height={"100px"}
 								alt={data.companylogo}
 							/>
 						</div>
